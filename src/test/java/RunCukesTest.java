d8a93aa3776e63c599f7fd2eeb617fdd77dc5904
jags <jagadeesh_is@yahoo.com>
2018-03-27 22:38:28 +0100

    jags changes

--- src/test/java/RunCukesTest.java
+++ src/test/java/RunCukesTest.java
@@ -1,14 +1,27 @@
+
+
 import cucumber.api.CucumberOptions;
+import cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.junit.runner.RunWith;
 
-@CucumberOptions(monochrome = true, format = { "pretty", "html:target/cucumber", "json:target/cucumber.json" },
-        tags = {"@hmp"},features = "src/test/resources/feature")
-public class RunCukesTest extends AbstractTestNGCucumberTests {
-
-}
-
-//@RunWith(Cucumber.class)
-//@CucumberOptions(format={"pretty","json:target/"} , features="/src/test/java/cucumber/features")
-//public class Launcher {
+//@CucumberOptions(monochrome = true, format = { "pretty", "html:target/cucumber", "json:target/cucumber.json" },
+//        tags = {"@hmp"},features = "/Volumes/D Drive/Gdrive/GIThubProjects/jagsEvergreen/src/test/resources/feature")
+//public class RunCukesTest extends AbstractTestNGCucumberTests {
 //
 //}
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        format = {
+                "json:target/cucumber/wikipedia.json",
+                "html:target/cucumber/wikipedia.html",
+                "pretty"
+        },
+        tags = {"hmp"}
+)
+
+public class RunCukesTest {
+}
