9b77c3a467bdef79d6e56c59d8b91b6d4d114faf
jagsauto <jagadeesh_is@yahoo.com>
2018-06-01 20:32:20 +0100

    Deleted Homeoffce Folder

--- src/main/java/TestNg/WebDriverSimple.java
+++ src/main/java/TestNg/WebDriverSimple.java
@@ -8,7 +8,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class WebDriverSimple {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/Volumes/D Drive/Gdrive/jagsEvergreenPractice/src/test/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
         driver.quit();
