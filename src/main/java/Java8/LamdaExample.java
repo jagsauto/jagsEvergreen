cf77d19ce206a41c604dc797f829ec388ff7fedc
jagsauto <jagadeesh_is@yahoo.com>
2019-02-04 08:54:46 +0000

    commit

---
+++ src/main/java/Java8/LamdaExample.java
@@ -0,0 +1,21 @@
+package Java8;
+
+import sun.rmi.rmic.Names;
+
+public class LamdaExample {
+
+/*
+public static void main(String args[]){
+
+    *//*
+
+    i.e ( ) -> { do something } ,(int a) -> { do something }, (int a, int b ,……,n) ->{do something }
+     *//*
+
+    Runnable runnable = () -> {
+        System.out.println("You hit it!");
+    };
+
+}*/
+
+}
