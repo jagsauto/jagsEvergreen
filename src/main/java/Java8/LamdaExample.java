866fb74d69625d42a1a2991a3e0c7e0716d5e8d4
jagsauto <jagadeesh_is@yahoo.com>
2019-04-19 11:27:37 +0100

    TodayChanges

--- src/main/java/Java8/LamdaExample.java
+++ src/main/java/Java8/LamdaExample.java
@@ -1,21 +1,39 @@
 package Java8;
 
-import sun.rmi.rmic.Names;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.jayway.restassured.RestAssured.given;
 
 public class LamdaExample {
 
-/*
-public static void main(String args[]){
 
-    *//*
+    public static void main(String args[]) {
 
-    i.e ( ) -> { do something } ,(int a) -> { do something }, (int a, int b ,……,n) ->{do something }
-     *//*
+        List<String> myList = new ArrayList<>();
+        Map<String,String> myMap = new HashMap<>();
 
-    Runnable runnable = () -> {
-        System.out.println("You hit it!");
-    };
+        String restAPI = "https://api.domainsdb.info/search?query=facebook&tld=com";
 
-}*/
+        myList = given().when().get(restAPI).jsonPath().get("domains.domain");
+
+        given().when().get(restAPI).jsonPath().get("domains");
+
+
+
+        // myList.forEach(puka -> System.out.println(puka));
+
+        myList.forEach(puka -> System.out.println(puka.contains(".fr")+"  "+puka)) ;
+
+        myList = myList.stream().filter(mylist -> mylist.contains(".de")).collect(Collectors.toList());
+
+        //  myList.forEach(puka -> System.out.println(puka));
+
+
+    }
 
 }
